047b119b3d2d3e354b7b9d183659d272af25309d
Damian Malysza <damian.malysza@gmail.com>
2020-10-27 23:33:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,20 +4,20 @@ class School
 
   def initialize(name)
     @name = name
-    @roster = {}
+    @@roster = {}
   end
 
   def add_student(name,grade)
     if roster.keys.include?(grade)
-      @roster[grade] << name
+      @@roster[grade] << name
     else
-      @roster[grade] = []
-      @roster[grade] << name
+      @@roster[grade] = []
+      @@roster[grade] << name
     end
   end
 
   def grade(grade)
-    @roster[grade]
+    @@roster[grade]
   end
 
   def sort
